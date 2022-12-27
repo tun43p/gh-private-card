945449592d6f5bd5f28f493feebfc6fd4d8cf196
tun43p <git@tun43p.com>
2022-12-27 12:27:33 +0100

    feat: build rendered html file

--- src/helpers/html.rs
+++ src/helpers/html.rs
@@ -1,3 +1,9 @@
+use std::{
+    fs::{create_dir_all, remove_dir_all, File},
+    io::Write,
+    path::Path,
+};
+
 use lazy_static::lazy_static;
 use tera::{Context, Tera};
 
@@ -30,12 +36,32 @@ pub fn create_github_repository_card(repository: &Repository) -> String {
     context.insert("issues", &repository.open_issues_count);
     context.insert("url", &repository.html_url);
 
-    // TODO(tun43p): Convert as image
+    create_html_file(&context, "github_repository.html".to_string())
+        .expect("error creating html file");
+
+    // TODO(tun43p): Return an image
     match TEMPLATES.render("github_repository.html", &context) {
-        Ok(template) => template,
+        Ok(html) => html,
         Err(error) => {
             eprintln!("{}", error.to_string());
-            error.to_string()
+            ::std::process::exit(1);
+        }
+    }
+}
+
+fn create_html_file(context: &Context, template: String) -> std::io::Result<()> {
+    if Path::new("build/").exists() {
+        remove_dir_all("build").expect("error deleting build directory");
+    }
+
+    create_dir_all("build/").expect("error creating build directory");
+    let mut file = File::create("build/render.html").expect("error creating file");
+
+    match TEMPLATES.render(&template, &context) {
+        Ok(html) => file.write_all(html.as_bytes()),
+        Err(error) => {
+            eprintln!("{}", error.to_string());
+            ::std::process::exit(1);
         }
     }
 }
